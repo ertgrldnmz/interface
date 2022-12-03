82297908dcff5adec105ade073b629e9cc4292e2
çağrı dönmez <ertgrldnmz@outlook.com>
2022-12-03 12:21:05 +0300

    add files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        FileLogger fileLogger=new FileLogger();
+        fileLogger.WriteLog();
+
+        DataBaseLogger dataBaseLogger=new DataBaseLogger();
+        dataBaseLogger.WriteLog();
+
+        SmsLogger smsLogger=new SmsLogger();
+        smsLogger.WriteLog();
+
+        LogManager logManager=new LogManager(new DataBaseLogger());
+        logManager.WriteLog();
+        
+    }
+}
