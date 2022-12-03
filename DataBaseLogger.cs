0e7d49f8cb6d6a8580dcff6cb11a438fd57c6d65
çağrı dönmez <ertgrldnmz@outlook.com>
2022-12-03 12:23:00 +0300

    added files.

---
+++ DataBaseLogger.cs
@@ -0,0 +1,8 @@
+public class DataBaseLogger : ILogger
+{
+    public void WriteLog()
+    {
+        //throw new NotImplementedException();
+        Console.WriteLine("DataBase'e Log Yapıldı.");
+    }
+}
---
+++ FileLogger.cs
@@ -0,0 +1,8 @@
+public class FileLogger : ILogger
+{
+    public void WriteLog()
+    {
+       // throw new NotImplementedException();
+       Console.WriteLine("Dosyaya Log yapıldı.");
+    }
+}
